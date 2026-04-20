3194db889370bee9c2b8323ccbf36caa5413607f
zerogex-options <zerogexoptions@gmail.com>
2026-04-20 14:26:29 -0400

    Add local typings for node:sqlite

---
+++ frontend/types/node-sqlite.d.ts
@@ -0,0 +1,15 @@
+declare module 'node:sqlite' {
+  export type SqliteValue = string | number | bigint | Uint8Array | null;
+
+  export interface StatementSync {
+    run(...values: SqliteValue[]): unknown;
+    get(...values: SqliteValue[]): unknown;
+    all(...values: SqliteValue[]): unknown[];
+  }
+
+  export class DatabaseSync {
+    constructor(path: string);
+    exec(sql: string): void;
+    prepare(sql: string): StatementSync;
+  }
+}
